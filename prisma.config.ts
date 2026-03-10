e110c3f02ea72fc4887d52147cf1c67025f4d755
cadekane <89505252+cadekane@users.noreply.github.com>
2026-03-10 01:07:59 -1000

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import 'dotenv/config';
+import { config } from 'dotenv';
+config({ path: '.env.local', override: true });
 import { defineConfig, env } from 'prisma/config';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!, // make sure .env has this
-});
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'),
-    // Prisma 7 does not support direct URLs for PostgreSQL
-    // directUrl: env('POSTGRES_URL_NON_POOLING'),
+    url: env('POSTGRES_PRISMA_URL'),
   },
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
