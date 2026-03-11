a4e26bed5ceeb50d973de0690d76e655b5d3a818
cadekane <89505252+cadekane@users.noreply.github.com>
2026-03-10 20:49:54 -1000

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { config } from 'dotenv';
-config({ path: '.env.local', override: true });
+import * as fs from 'fs';
+
+if (fs.existsSync('.env.local')) {
+  config({ path: '.env.local', override: true });
+}
+
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('POSTGRES_PRISMA_URL'),
+    url: env('DATABASE_URL'),
   },
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
